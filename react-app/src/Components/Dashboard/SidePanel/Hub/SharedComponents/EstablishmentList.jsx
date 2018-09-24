acc3df50de04766c2d27fca7f0cb7fbfd04a8fcd
alejo4373 <alejo4373@gmail.com>
2018-09-24 11:48:25 -0400

    establishment card/photo overflow fix

--- react-app/src/Components/Dashboard/SidePanel/Hub/SharedComponents/EstablishmentList.jsx
+++ react-app/src/Components/Dashboard/SidePanel/Hub/SharedComponents/EstablishmentList.jsx
@@ -8,26 +8,28 @@ import '../../../../../Stylesheets/establishment-list.css'
 class EstablishmentList extends Component {
   render() {
     const { establishments } = this.props;
-    return(
+    return (
       <div className='establishment-list'>
         <h4>Establishments</h4>
         <ul>
           {
             establishments.map((est, i) => {
-              return(
-                <Link to={`/establishments/${est.id}`} key={i}>
-                  <div 
-                    className='establishment-card'
-                    to={`/establishments/${est.id}`}
-                  >
-                    <div className='left' style={{backgroundImage: `url(${est.googlePhotoUrl})`}}>
+              return (
+                <li>
+                  <Link to={`/establishments/${est.id}`} key={i}>
+                    <div
+                      className='establishment-card'
+                      to={`/establishments/${est.id}`}
+                    >
+                      <div className='left' style={{ backgroundImage: `url(${est.googlePhotoUrl})` }}>
+                      </div>
+                      <div className='right'>
+                        <p className='name'>{est.displayName}</p>
+                        <p>{est.phone}</p>
+                      </div>
                     </div>
-                    <div className='right'>
-                      <p className='name'>{est.displayName}</p> 
-                      <p>{est.phone}</p>
-                    </div> 
-                  </div>
-                </Link>
+                  </Link>
+                </li>
               )
             })
           }
