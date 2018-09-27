8cf8d0cd61632b725e12df7e3c5e8c39eaffaa9b
alejo4373 <alejo4373@gmail.com>
2018-09-27 10:28:21 -0400

    google image url fix for markers in map

--- react-app/src/Components/Dashboard/MapComponent.jsx
+++ react-app/src/Components/Dashboard/MapComponent.jsx
@@ -23,12 +23,12 @@ class MapComponent extends Component {
 
   tryGetClientLocation = () => {
     const onSuccess = position => {
-        this.setState({
-          clientLocation: {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          }
-        })
+      this.setState({
+        clientLocation: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+      })
     }
     const onError = err => {
       return console.log('Error getting client location', err)
@@ -53,9 +53,9 @@ class MapComponent extends Component {
     }
     console.log('boundsArea', areaBounds)
     console.log('zoom', zoom)
-    if(zoom >= 14) {
+    if (zoom >= 14) {
       getEstablishmentsInBounds(areaBounds, (err, establishments) => {
-        if(err) {
+        if (err) {
           return console.log('error getEstablishmentsInBounds:', err)
         }
         this.setState({
@@ -65,67 +65,82 @@ class MapComponent extends Component {
     }
   }
 
+  handleMapReady = (mapProps, map) => {
+    let { google } = mapProps;
+    // Create PlacesService to retrieve images src from google
+    this.placesService = new google.maps.places.PlacesService(map);
+  }
+
   onMakerClick = (props, marker, e) => {
-    console.log('on pin click')
-    console.log(props)
-    console.log(marker)
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showInfoWindow: true
+    const { establishment } = props
+
+    // Get the photo url for the active marker from google. This sucks, google won't allow me to store 
+    // urls to their images in my database.
+    this.placesService.getDetails({
+      placeId: establishment.googlePlaceId,
+      fields: ['photo']
+    }, (place, status) => {
+      if (status === 'OK') {
+        establishment.googlePhotoUrl = place.photos[0].getUrl({ maxWidth: 500, maxHeight: 500 });
+      } else {
+        establishment.googlePhotoUrl = 'https://images.pexels.com/photos/681847/pexels-photo-681847.jpeg?dl&fit=crop&crop=entropy&w=400&h='
+      }
+
+      this.setState({
+        selectedPlace: establishment,
+        activeMarker: marker,
+        showInfoWindow: true
+      })
     })
   }
 
   render() {
-    const { clientLocation, establishments, activeMarker, showInfoWindow, selectedPlace  } = this.state
-    return(
+    const { clientLocation, establishments, activeMarker, showInfoWindow, selectedPlace } = this.state
+    return (
       <div className='map-container'>
-        <Map 
-          google={this.props.google} 
+        <Map
+          google={this.props.google}
           zoom={14}
-          style={{width: 'inherit'}}
+          style={{ width: 'inherit' }}
+          onReady={this.handleMapReady}
           onTilesloaded={this.handleTilesLoaded}
           center={clientLocation}
-          containerStyle={{width: 'inherit', position: 'relative'}}
+          containerStyle={{ width: 'inherit', position: 'relative' }}
         >
           <Marker
-              title='You'
-              position={clientLocation}
-              icon={{
-                    url: "http://i.stack.imgur.com/orZ4x.png",
-                    scaledSize: new this.props.google.maps.Size(22,22)
-              }}
+            title='You'
+            position={clientLocation}
+            icon={{
+              url: "http://i.stack.imgur.com/orZ4x.png",
+              scaledSize: new this.props.google.maps.Size(22, 22)
+            }}
           />
 
-        {
-          establishments.map((est, i) => (
+          {
+            establishments.map((est, i) => (
               <Marker
                 key={i}
-                title={est.displayName}
-                estDisplayName={est.displayName}
-                estImage={est.googlePhotoUrl}
-                estId={est.id}
-                estPhone={est.phone}
-                position={{lat: est.lat, lng: est.lng}}
+                establishment={est}
+                position={{ lat: est.lat, lng: est.lng }}
                 onClick={this.onMakerClick}
               />
-          ))
-        }
-        <InfoWindow marker={activeMarker} visible={showInfoWindow} >
-          <a href={`/establishments/${selectedPlace.estId}`} >
-            <div 
-              className='establishment-card'
-              to={`/establishments/${selectedPlace.estId}`}
-            >
-              <div className='left' style={{backgroundImage: `url(${selectedPlace.estImage})`}}>
+            ))
+          }
+          <InfoWindow marker={activeMarker} visible={showInfoWindow} >
+            <a href={`/establishments/${selectedPlace.id}`} >
+              <div
+                className='establishment-card'
+                to={`/establishments/${selectedPlace.id}`}
+              >
+                <div className='left' style={{ backgroundImage: `url(${selectedPlace.googlePhotoUrl})` }}>
+                </div>
+                <div className='right'>
+                  <p className='name'>{selectedPlace.displayName}</p>
+                  <p>{selectedPlace.phone}</p>
+                </div>
               </div>
-              <div className='right'>
-                <p className='name'>{selectedPlace.estDisplayName}</p> 
-                <p>{selectedPlace.estPhone}</p>
-              </div> 
-            </div>
-          </a>
-        </InfoWindow>
+            </a>
+          </InfoWindow>
         </Map>
       </div>
     )
