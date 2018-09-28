8df9f9147922e3b52701b216bccc68d7c40820fd
alejo4373 <alejo4373@gmail.com>
2018-09-28 14:41:46 -0400

    aws setup code split

---
+++ AWS/setup-scripts/dynamoDB.js
@@ -0,0 +1,93 @@
+const { AWS } = global;
+
+let db = new AWS.DynamoDB()
+
+// Just so that we can use await instead of nesting callbacks
+const createTableWithPromise = (tableParams) => {
+  return new Promise((resolve, reject) => {
+    db.createTable(tableParams, (err, data) => {
+      if (err) { reject(err) }
+      resolve(data)
+    })
+  })
+}
+
+let establishmentsTableParams = {
+  TableName: 'foobar_establishments_table',
+  AttributeDefinitions: [
+    {
+      AttributeName: 'id',
+      AttributeType: 'S'
+    },
+    {
+      AttributeName: 'managerUsername',
+      AttributeType: 'S'
+    }
+  ],
+  KeySchema: [
+    {
+      AttributeName: 'id',
+      KeyType: 'HASH'
+    },
+    {
+      AttributeName: 'managerUsername',
+      KeyType: 'RANGE'
+    }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5
+  }
+}
+
+let eventsTableParams = {
+  TableName: 'foobar_events_table',
+  AttributeDefinitions: [
+    {
+      AttributeName: 'id',
+      AttributeType: 'S'
+    },
+    {
+      AttributeName: 'leagueId',
+      AttributeType: 'S'
+    }
+  ],
+  KeySchema: [
+    {
+      AttributeName: 'id',
+      KeyType: 'HASH'
+    },
+    {
+      AttributeName: 'leagueId',
+      KeyType: 'RANGE'
+    }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5
+  }
+}
+
+const main = () => {
+  // let estTableCheck = await checkTable('foobar_establishments_table')
+  createTableWithPromise(establishmentsTableParams)
+    .then(data => {
+      console.log('Creating table', data.TableDescription.TableName)
+    })
+    .catch(err => {
+      console.log('[Error]', err.message)
+    });
+
+  createTableWithPromise(eventsTableParams)
+    .then(data => {
+      console.log('Creating table', data.TableDescription.TableName)
+    })
+    .catch(err => {
+      console.log('[Error]', err.message)
+    });
+
+  // if(estTableCheck
+  // let eventsTable = createTableWithPromises(eventsTableParams);
+}
+
+module.exports = main;
--- aws-setup.js
+++ aws-setup.js
@@ -1,94 +1,21 @@
-const AWS = require('aws-sdk');
+// Attach AWS to global object
+global.AWS = require('aws-sdk');
+
+const { AWS } = global;
+
+// Set APIs versions
+AWS.config.apiVersions = {
+  cognitoidentity: '2014-06-30',
+  dynamodb: '2012-08-10',
+};
+
+// Loads credentials and region
 AWS.config.loadFromPath('./aws-config.json');
 
-let db = new AWS.DynamoDB({ apiVersion: '2012-08-10' })
-
-// Just so that we can use await instead of nesting callbacks
-const createTableWithPromise = (tableParams) => {
-  return new Promise((resolve, reject) => {
-    db.createTable(tableParams, (err, data) => {
-      if (err) { reject(err) }
-      resolve(data)
-    })
-  })
-}
-
-let establishmentsTableParams = {
-  TableName: 'foobar_establishments_table',
-  AttributeDefinitions: [
-    {
-      AttributeName: 'id',
-      AttributeType: 'S'
-    },
-    {
-      AttributeName: 'managerUsername',
-      AttributeType: 'S'
-    }
-  ],
-  KeySchema: [
-    {
-      AttributeName: 'id',
-      KeyType: 'HASH'
-    },
-    {
-      AttributeName: 'managerUsername',
-      KeyType: 'RANGE'
-    }
-  ],
-  ProvisionedThroughput: {
-    ReadCapacityUnits: 5,
-    WriteCapacityUnits: 5
-  }
-}
-
-let eventsTableParams = {
-  TableName: 'foobar_events_table',
-  AttributeDefinitions: [
-    {
-      AttributeName: 'id',
-      AttributeType: 'S'
-    },
-    {
-      AttributeName: 'leagueId',
-      AttributeType: 'S'
-    }
-  ],
-  KeySchema: [
-    {
-      AttributeName: 'id',
-      KeyType: 'HASH'
-    },
-    {
-      AttributeName: 'leagueId',
-      KeyType: 'RANGE'
-    }
-  ],
-  ProvisionedThroughput: {
-    ReadCapacityUnits: 5,
-    WriteCapacityUnits: 5
-  }
-}
+const setupDynamoDB = require('./AWS/setup-scripts/dynamoDB')
 
 const main = () => {
-  // let estTableCheck = await checkTable('foobar_establishments_table')
-  createTableWithPromise(establishmentsTableParams)
-    .then(data => {
-      console.log('Creating table', data.TableDescription.TableName)
-    })
-    .catch(err => {
-      console.log('[Error]', err.message)
-    });
-
-  createTableWithPromise(eventsTableParams)
-    .then(data => {
-      console.log('Creating table', data.TableDescription.TableName)
-    })
-    .catch(err => {
-      console.log('[Error]', err.message)
-    });
-
-  // if(estTableCheck
-  // let eventsTable = createTableWithPromises(eventsTableParams);
+  setupDynamoDB();
 }
 
 main();
