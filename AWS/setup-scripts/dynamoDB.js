9525e0b642d7ce470945f2347cf2341fa2c15847
alejo4373 <alejo4373@gmail.com>
2018-10-06 15:05:52 -0400

    dynamo script create tables concurrently

--- AWS/setup-scripts/dynamoDB.js
+++ AWS/setup-scripts/dynamoDB.js
@@ -3,7 +3,7 @@ let { setGlobalVar } = require('./utils');
 let db = new AWS.DynamoDB()
 
 // Just so that we can use await instead of nesting callbacks
-const createTableWithPromise = (tableParams) => {
+const awsCreateTable = (tableParams) => {
   return new Promise((resolve, reject) => {
     db.createTable(tableParams, (err, data) => {
       if (err) { reject(err) }
@@ -14,7 +14,7 @@ const createTableWithPromise = (tableParams) => {
   })
 }
 
-const describeTable = (params) => {
+const awsDescribeTable = (params) => {
   return new Promise((resolve, reject) => {
     db.describeTable(params, (err, data) => {
       if (err) { reject(err) }
@@ -85,41 +85,45 @@ let eventsTableParams = {
 const tableAlreadyExists = async (tableName) => {
   let table;
   try {
-    table = await describeTable({ TableName: tableName });
+    table = await awsDescribeTable({ TableName: tableName });
   } catch (err) {
     return null
   }
   return table;
 }
 
+const createTable = (tableParams) => {
+  return new Promise((resolve, reject) => {
+    tableAlreadyExists(tableParams.TableName)
+      .then(table => {
+        console.log(`Table with name ${table.TableName} already exists. Skipping...`)
+        setGlobalVar([`${table.TableName}Arn`], table.TableArn)
+        resolve(true);
+      })
+      .catch(err => {
+        awsCreateTable(tableParams)
+          .then(table => {
+            console.log('Creating table', table.TableName)
+            setGlobalVar([`${table.TableName}Arn`], table.TableArn)
+            resolve(true);
+          })
+          .catch(err => {
+            console.log('[Error]', err)
+            reject(err)
+          });
+      })
+  })
+}
+
 const main = async () => {
-  let establishmentsTable = await tableAlreadyExists(establishmentsTableParams.TableName);
-  let eventsTable = await tableAlreadyExists(eventsTableParams.TableName);
+  let establishmentsTablePromise = createTable(establishmentsTableParams)
+  let eventsTablePromise = createTable(eventsTableParams)
 
-  if (!establishmentsTable) {
-    try {
-      establishmentsTable = await createTableWithPromise(establishmentsTableParams)
-      console.log('Creating table', establishmentsTable.TableName)
-      setGlobalVar('establishmentsTableArn', establishmentsTable.TableArn)
-    } catch (err) {
-      console.log('[Error]', err)
-    };
-  } else {
-    console.log(`Table with name ${establishmentsTable.TableName} already exists. Skipping...`)
-    setGlobalVar('establishmentsTableArn', establishmentsTable.TableArn)
-  }
-
-  if (!eventsTable) {
-    try {
-      eventsTable = await createTableWithPromise(eventsTableParams);
-      console.log('Creating table', eventsTable.TableName)
-      setGlobalVar('eventsTableArn', eventsTable.TableArn)
-    } catch (err) {
-      console.log('[Error]', err)
-    };
-  } else {
-    console.log(`Table with name ${eventsTable.TableName} already exists. Skipping...`)
-    setGlobalVar('eventsTableArn', eventsTable.TableArn)
+  try {
+    // Concurrently create tables
+    await Promise.all([establishmentsTablePromise, eventsTablePromise])
+  } catch (err) {
+    console.log('Promise.all err:', err);
   }
 }
 
