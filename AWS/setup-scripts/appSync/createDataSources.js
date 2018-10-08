92b8a23ee96a1a66e7bf74462df10f9289272c87
alejo4373 <alejo4373@gmail.com>
2018-10-07 23:01:01 -0400

    create data source now reusable function

--- AWS/setup-scripts/appSync/createDataSources.js
+++ AWS/setup-scripts/appSync/createDataSources.js
@@ -1,45 +1,85 @@
 const appSync = new AWS.AppSync();
+const iam = require('../iam')
 
 const createDataSource = (params) => {
   return new Promise((resolve, reject) => {
     appSync.createDataSource(params, (err, data) => {
       if (err) { reject(err) }
-      else { resolve(data) }
+      else { resolve(data.dataSource) }
     })
   })
 }
 
-const main = async ({ name, apiId, type, typeConfig, serviceRoleArn }) => {
-  let params = {
-    apiId,
-    name,
-    type,
-    serviceRoleArn
-  };
-
-  switch (type) {
-    case 'AWS_LAMBDA':
-      params.lambdaConfig = {
-        lambdaFunctionArn: typeConfig.lambdaFunctionArn
-      }
-      break;
-
-    case 'AMAZON_DYNAMODB':
-      params.dynamodbConfig = {
-        awsRegion: AWS.config.region,
-        tableName: typeConfig.tableName,
-        useCallerCredentials: true
-      }
-      break;
-  }
-
-  let dataSource;
-  try {
-    dataSource = await createDataSource(params);
-    return dataSource;
-  } catch (err) {
-    console.log('[Error]', err)
-  }
+const getDataSource = (apiId, name) => {
+  const params = { apiId, name }
+  return new Promise((resolve, reject) => {
+    appSync.getDataSource(params, (err, data) => {
+      if (err) { reject(err) }
+      else { resolve(data.dataSource) }
+    })
+  })
 }
 
+// Creates a data source or retrieves it if already exists and returns it
+const main = async ({ type, apiId, dataSourceName, dataSourceArn }) => {
+  // See if a data source already exists. If not create it, otherwise skip
+  let dataSource;
+  try {
+    dataSource = await getDataSource(apiId, dataSourceName);
+  } catch (err) {
+    if (err.code === 'NotFoundException') {
+      dataSource = null;
+    } else {
+      console.log('[Error]', err)
+    }
+  }
+
+  if (!dataSource) {
+    let params = {
+      apiId,
+      type,
+      name: dataSourceName,
+      serviceRoleArn: null // Will be set once the role is created
+    };
+
+    switch (type) {
+      case 'AWS_LAMBDA':
+        params.lambdaConfig = {
+          lambdaFunctionArn: dataSourceName
+        }
+        try {
+          // TODO create role for lambda
+          params.serviceRoleArn = await iam.createRoleForAppSyncToAccessDataSource('lambdaFunction', dataSourceName, dataSourceArn)
+        } catch (err) {
+          console.log('[Error]', err)
+        }
+        break;
+
+      case 'AMAZON_DYNAMODB':
+        params.dynamodbConfig = {
+          awsRegion: AWS.config.region,
+          tableName: dataSourceName,
+          useCallerCredentials: true
+        }
+        // Create the appropriate role to allow access to data source.
+        try {
+          params.serviceRoleArn = await iam.createRoleForAppSyncToAccessDataSource('dynamoDBTable', dataSourceName, dataSourceArn)
+        } catch (err) {
+          console.log('[Error]', err)
+        }
+        break;
+    }
+
+    try {
+      dataSource = await createDataSource(params);
+      console.log(`Data source with name: ${dataSource.name} created. Success`)
+    } catch (err) {
+      console.log('[Error]', err)
+    }
+  } else {
+    console.log(`Data source with name: ${dataSource.name} already exists. Skipping `)
+  }
+
+  return dataSource;
+}
 module.exports = main
