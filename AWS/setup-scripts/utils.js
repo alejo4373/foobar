bd88ba4c8f7b2ee65bb7f4a2100d46c39fb2067d
alejo4373 <alejo4373@gmail.com>
2018-10-09 21:00:46 -0400

    export global vars to .env file

--- AWS/setup-scripts/utils.js
+++ AWS/setup-scripts/utils.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const setGlobalVar = (name, value) => {
   global.aws_vars = {
     [name]: value,
@@ -5,6 +7,26 @@ const setGlobalVar = (name, value) => {
   }
 }
 
+const exportEnvVarsFile = () => {
+  let prefix = 'REACT_APP_' // Since the .env file will be used by a React App bootstrapped with create-react-app
+  let cognito = global.aws_vars.cognito;
+  let apiEndpoint = global.aws_vars.api.GRAPHQL_ENDPOINT;
+
+  let fileLines = [
+    `${prefix}GRAPHQL_ENDPOINT=${apiEndpoint}`,
+    `${prefix}GOOGLE_MAPS_API_KEY=${process.env.GOOGLE_MAPS_API_KEY}` //Bring google maps api key to this other .env file as well
+  ]
+
+  for (key in cognito) {
+    fileLines.push(`${prefix + key}=${cognito[key]}`)
+  }
+
+  // Format and write to file
+  let fileContent = fileLines.join('\n');
+  fs.writeFileSync('./react-app/.env.new', fileContent, 'utf8')
+}
+
 module.exports = {
   setGlobalVar,
+  exportEnvVarsFile
 }
