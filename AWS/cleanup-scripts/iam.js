8ac19fc98877b5efe01be00c1817fa0e6f5cf60c
alejo4373 <alejo4373@gmail.com>
2018-11-22 17:06:10 -0500

    deleting all created policies

--- AWS/cleanup-scripts/iam.js
+++ AWS/cleanup-scripts/iam.js
@@ -14,13 +14,34 @@ const detachRolePolicy = (params) => {
     })
   })
 }
+
+const deletePolicy = (params) => {
+  return new Promise((resolve, reject) => {
+    iam.deletePolicy(params, (err, data) => {
+      if (err) { reject(err) }
+      else { resolve(data) }
+    })
+  })
+}
+
 const detachPoliciesFromRoles = async (IAMRolesAndPolicies) => {
   let detachPromises = IAMRolesAndPolicies.map(elem => detachRolePolicy(elem));
   try {
     await Promise.all(detachPromises);
     console.log('Detached all policies from all roles.');
   } catch (err) {
-    console.log('[Error] => Detaching all policies from all roles.');
+    console.log('[Error] => Detaching all policies from all roles.', err);
+  }
+}
+
+const deletePolicies = async (IAMRolesAndPolicies) => {
+  let policies = IAMRolesAndPolicies.map(elem => ({PolicyArn: elem.PolicyArn}));
+  let deletePolicesPromises = policies.map(elem => deletePolicy(elem));
+  try {
+    await Promise.all(deletePolicesPromises);
+    console.log('Deleted all policies.');
+  } catch (err) {
+    console.log('[Error] => Deleting all policies.', err);
   }
 }
 
@@ -28,9 +49,12 @@ const main = async ({ IAMRolesAndPolicies }) => {
   if (IAMRolesAndPolicies !== undefined && IAMRolesAndPolicies.length) {
     try {
       await detachPoliciesFromRoles(IAMRolesAndPolicies);
+      await deletePolicies(IAMRolesAndPolicies);
     } catch (err) {
-      console.log('[Error] => Cleaning up IAM roles and policies.');
+      console.log('[Error] => Cleaning up IAM roles and policies.', err);
     }
+  } else {
+      console.log('[Warning] => Nothing to clean-up. Is awsResourcesCreated.json empty or with no IAM roles and policies?.');
   }
 }
 module.exports = main;
