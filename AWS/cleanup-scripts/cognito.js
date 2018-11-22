01f0882d3903efc83568e812bf7a90b04125a362
alejo4373 <alejo4373@gmail.com>
2018-11-22 13:53:05 -0500

    delete cognito identity pool

--- AWS/cleanup-scripts/cognito.js
+++ AWS/cleanup-scripts/cognito.js
@@ -1,10 +1,22 @@
-const cognito_idp = new AWS.CognitoIdentityServiceProvider();
-const cognito_identity = new AWS.CognitoIdentity();
+const cognitoIDP = new AWS.CognitoIdentityServiceProvider();
+const cognitoIdentity = new AWS.CognitoIdentity();
 
 const deleteUserPool = (userPoolId) => {
   return new Promise((resolve, reject) => {
     let params = { UserPoolId: userPoolId };
-    cognito_idp.deleteUserPool(params, (err, data) => {
+    cognitoIDP.deleteUserPool(params, (err, data) => {
+      if (err) { reject(err) }
+      else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+const deleteIdentityPool = (identityPoolId) => {
+  return new Promise((resolve, reject) => {
+    let params = { IdentityPoolId: identityPoolId };
+    cognitoIdentity.deleteIdentityPool(params, (err, data) => {
       if (err) { reject(err) }
       else {
         resolve(data)
@@ -14,13 +26,28 @@ const deleteUserPool = (userPoolId) => {
 }
 
 const main = async ({ userPool, identityPool }) => {
-  if (userPool.id) {
+  if (identityPool) {
+    try {
+      await deleteIdentityPool(identityPool.id);
+      console.log(`Removing identity pool with name: ${identityPool.name}, id: ${identityPool.id}`);
+    } catch (err) {
+      if (err.code === 'ResourceNotFoundException') {
+        console.log(`Identity pool ${identityPool.name} doesn't exists`);
+      } else {
+        console.log('[Error] => Deleting Identity pool', err);
+      }
+    }
+  } else {
+    console.log('No Identity pool to delete');
+  }
+
+  if (userPool) {
     try {
       await deleteUserPool(userPool.id);
       console.log(`Removing user pool with name: ${userPool.name}, id: ${userPool.id}`);
     } catch (err) {
       if (err.code === 'ResourceNotFoundException') {
-        console.log(`User pool ${lambdaFunction} doesn't exists`);
+        console.log(`User pool ${userPool.name} doesn't exists`);
       } else {
         console.log('[Error] => Deleting User pool', err);
       }
