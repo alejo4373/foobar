f2ff45fc556ac5c068d2a0ad8d26717bf5e565b2
alejo4373 <alejo4373@gmail.com>
2018-11-22 17:23:27 -0500

    delete roles, completing the AWS IAM clean-up

--- AWS/cleanup-scripts/iam.js
+++ AWS/cleanup-scripts/iam.js
@@ -24,37 +24,62 @@ const deletePolicy = (params) => {
   })
 }
 
+const deleteRole = (params) => {
+  return new Promise((resolve, reject) => {
+    iam.deleteRole(params, (err, data) => {
+      if (err) { reject(err) }
+      else { resolve(data) }
+    })
+  })
+}
+
 const detachPoliciesFromRoles = async (IAMRolesAndPolicies) => {
   let detachPromises = IAMRolesAndPolicies.map(elem => detachRolePolicy(elem));
   try {
     await Promise.all(detachPromises);
     console.log('Detached all policies from all roles.');
   } catch (err) {
-    console.log('[Error] => Detaching all policies from all roles.', err);
+    throw err;
   }
 }
 
-const deletePolicies = async (IAMRolesAndPolicies) => {
-  let policies = IAMRolesAndPolicies.map(elem => ({PolicyArn: elem.PolicyArn}));
+const deletePolicies = async (policies) => {
   let deletePolicesPromises = policies.map(elem => deletePolicy(elem));
   try {
     await Promise.all(deletePolicesPromises);
     console.log('Deleted all policies.');
   } catch (err) {
-    console.log('[Error] => Deleting all policies.', err);
+    throw err;
+  }
+}
+
+const deleteRoles = async (roles) => {
+  let deleteRolesPromises = roles.map(elem => deleteRole(elem));
+  try {
+    await Promise.all(deleteRolesPromises);
+    console.log('Deleted all roles.');
+  } catch (err) {
+    throw err;
   }
 }
 
 const main = async ({ IAMRolesAndPolicies }) => {
   if (IAMRolesAndPolicies !== undefined && IAMRolesAndPolicies.length) {
+    let policies = IAMRolesAndPolicies.map(elem => ({ PolicyArn: elem.PolicyArn }));
+    let roles = IAMRolesAndPolicies.map(elem => ({ RoleName: elem.RoleName }));
     try {
       await detachPoliciesFromRoles(IAMRolesAndPolicies);
-      await deletePolicies(IAMRolesAndPolicies);
+      await deletePolicies(policies);
+      await deleteRoles(roles);
     } catch (err) {
-      console.log('[Error] => Cleaning up IAM roles and policies.', err);
+      if (err.code === 'NoSuchEntity') {
+        console.log('[Error] => Cleaning up IAM roles and policies.', err.message);
+      } else {
+        console.log('[Error] => Cleaning up IAM roles and policies.', err);
+      }
     }
   } else {
-      console.log('[Warning] => Nothing to clean-up. Is awsResourcesCreated.json empty or with no IAM roles and policies?.');
+    console.log('[Warning] => Nothing to clean-up. Is awsResourcesCreated.json empty or with no IAM roles and policies?.');
   }
 }
 module.exports = main;
