5f7332bb8373c93ee8a74b02702b867e8a755baa
alejo4373 <alejo4373@gmail.com>
2018-12-09 20:20:03 -0500

    lambda function to index ests read from DDB stream

--- AWS/Lambda/foobar_lambda_DDB_ES_indexers/foobar_establishments_DDB_ES_indexer.js
+++ AWS/Lambda/foobar_lambda_DDB_ES_indexers/foobar_establishments_DDB_ES_indexer.js
@@ -1,25 +1,32 @@
 const AWS = require('aws-sdk');
 const region = AWS.config.region;
-let domain = 'search-es-testdomain-gucqhfplkzl4ny5yhbbxwdg24q.us-east-2.es.amazonaws.com'; 
-let index = 'movies';
-let type = '_doc';
+const domain = process.env.ES_DOMAIN
+const type = '_doc';
+const index = 'foobar-est-index'
 
-function indexDocument(document) {
+const manageIndex = (record, eventName) => {
   let endpoint = new AWS.Endpoint(domain);
   let request = new AWS.HttpRequest(endpoint, region);
-
-  request.method = 'PUT';
+  let document = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
+  let id = record.dynamodb.Keys.id.S;
+  if (eventName == 'REMOVE') {
+    request.method = 'DELETE';
+  } else {
+    request.method = 'PUT';
+  }
   request.path += index + '/' + type + '/' + id;
   request.body = JSON.stringify(document);
   request.headers['host'] = domain;
   request.headers['Content-Type'] = 'application/json';
   request.headers["Content-Length"] = request.body.length;
 
+  //Get and credentials and sign request
   let credentials = AWS.config.credentials;
   let signer = new AWS.Signers.V4(request, 'es');
   signer.addAuthorization(credentials, new Date());
-  let client = new AWS.HttpClient();
 
+  // Send the request
+  let client = new AWS.HttpClient();
   return new Promise((resolve, reject) => {
     client.handleRequest(request, null, function (response) {
       let responseBody = '';
@@ -30,24 +37,28 @@ function indexDocument(document) {
         resolve(responseBody);
       });
     }, (error) => {
-      console.log('error =>', error)
       reject(error);
     });
   })
 }
 
 exports.handler = async (event) => {
-  let records = event.Records;
+  const { Records } = event;
+  let res;
   let promises = [];
-  records.forEach(r => {
-    promises.push(indexDocument(r))
+  Records.forEach(record => {
+    const { eventName } = record;
+    promises.push(manageIndex(record, eventName))
   });
-  // TODO implement
-  let res = await Promise.all(promises);
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(res),
-  };
-  console.log('res ===>', res);
-  return response;
+  try {
+    res = await Promise.all(promises);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(res),
+    };
+    return response;
+  } catch (err) {
+    console.log('Error ===>', res);
+    return null;
+  }
 };
