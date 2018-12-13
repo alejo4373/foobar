6e7173ad4f5a8eb403f4c83829b9ebf432486507
alejo4373 <alejo4373@gmail.com>
2018-12-12 22:06:07 -0500

    await until ES domain is ready logic

--- AWS/setup-scripts/elasticSearch.js
+++ AWS/setup-scripts/elasticSearch.js
@@ -2,7 +2,46 @@ const { createRoleForLambdaToAccessES } = require('../setup-scripts/iam');
 // AWS must be in the global scope
 const ES = new AWS.ES({ apiVersion: '2015-01-01' })
 
-const createESDomain = (domainName, roleArn) => {
+const isDomainReady = (domainName) => {
+  console.log('Checking if ES domain is ready. Please wait.');
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      ES.describeElasticsearchDomain({ DomainName: domainName }, (err, data) => {
+        if (err) { reject(err) }
+        else {
+          if (data.DomainStatus.Processing === false) {
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        }
+      });
+    }, 1000 * 60 * 2);
+  });
+}
+
+// Ask if domain is ready every 2 minutes
+const waitUntilDomainReady = async (domainName) => {
+  console.log(`Waiting for domain ${domainName} to be ready...`);
+  console.log('[Note] New domains take up to ten minutes to initialize. After your domain is initialized, you can upload data and make changes to the domain.');
+  let domainReady = false;
+  while (!domainReady) {
+    try {
+      domainReady = await isDomainReady(domainName);
+    } catch (err) {
+      return console.log('[Error]', err);
+    }
+  }
+  console.log('Domain is ready for use in production');
+}
+
+/**
+ * Create Elasticsearch domain with access policy that allows
+ * access to the roleArn to interact with the domain
+ * @param {string} domainName The domain name to be used
+ * @param {string} roleArn Role ARN that will have access to domain
+ **/
+const createESDomain = async (domainName, roleArn) => {
   const domainAccessPolicy = {
     "Version": "2012-10-17",
     "Statement": [
@@ -41,15 +80,30 @@ const createESDomain = (domainName, roleArn) => {
       AutomatedSnapshotStartHour: 0
     },
   };
-  return ES.createElasticsearchDomain(esDomainParams).promise()         // successful response
+  try {
+    console.log('Creating ES domain');
+    let res = await ES.createElasticsearchDomain(esDomainParams).promise();
+
+    // If processing is not true, it means the domain was already created
+    // aws doesn't complain if trying to create a domain with the name of
+    // an already existing domain
+    if (res.DomainStatus.Processing === true) {
+      console.log('New ES domain created. Success');
+      await waitUntilDomainReady(domainName);
+    } else {
+      console.log(`Domain '${domainName}' already exists. Skipping...`)
+    }
+  } catch (err) {
+    throw err;
+  }
 }
+
 const main = async () => {
   let domainName = 'foobar-es-domain';
   let roleArn;
   try {
     roleArn = await createRoleForLambdaToAccessES();
-    let res = await createESDomain(domainName, roleArn);
-    console.log(res);
+    await createESDomain(domainName, roleArn);
   } catch (err) {
     console.log(err);
   }
